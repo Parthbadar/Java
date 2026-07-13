acd9a33f66a8a8ae5abd28c3c8eb17dc144cc0c1
Parthbadar <parthbadar9@gmail.com>
2026-07-13 17:11:34 +0530

    variables code

---
+++ variable1.java
@@ -0,0 +1,16 @@
+class variable1{
+    public static void main(String[] args){
+        
+        int a = 10;
+        float f = 3.14f;
+        double d = 5.56456;
+        String name = "parth";
+        Boolean b= true;
+
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(name);
+        System.out.println(b);
+    }
+}
