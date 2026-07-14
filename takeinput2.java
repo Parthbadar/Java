8857df864dbbcc41ddc12b37a54691441b304e02
Parthbadar <parthbadar9@gmail.com>
2026-07-14 18:41:44 +0530

    userinput and types

---
+++ takeinput2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// input in user and types.  example employe file madhe ajun
+class takeinput2{
+     public static void main(String[] args){
+
+        // class_name obj_name = new class_name();
+        Scanner sarthak = new Scanner(System.in);
+
+        // nextInt()
+        // nextFloat()
+        // nextDouble()
+        // next()
+        // nextLine()
+
+        // System.out.println("enter any number");
+        // int a = sarthak.nextInt();
+
+        // System.out.println(" number is " + a);
+
+        System.out.println("enter your name");
+        String name = sarthak.nextLine();
+
+        System.out.println(" My name is " + name);
+
+        
+
+
+     }
+}
