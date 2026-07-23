62f4ff0a679d3291fc35ce2f56552d6057f9707c
Parthbadar <parthbadar9@gmail.com>
2026-07-23 16:38:56 +0530

    for loop

---
+++ Forloop8.java
@@ -0,0 +1,8 @@
+public class Forloop8 {
+    public static void main(String[] parth){
+        for(int i = 10; i>=1; i-=1)
+        {
+            System.out.println("Hello parth, count " + i);
+        }
+}
+}
