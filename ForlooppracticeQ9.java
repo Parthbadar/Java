6cfeef1e6a0960a9373e54c494578b1fe00cf04a
Parthbadar <parthbadar9@gmail.com>
2026-07-24 18:34:02 +0530

    forloop practice Q

---
+++ ForlooppracticeQ9.java
@@ -0,0 +1,10 @@
+public class ForlooppracticeQ9 {
+    public static void main(String[] parth){
+        for(int i = 1; i<=5; i+=1)
+        {
+            int Squ = i*i;
+            int cube = i*i*i;
+            System.out.println("Data --> " + i +", SQR--->"+ Squ + ", cube--->" +cube );
+        }
+}
+}
