853b82e32175aa84e1a9a164701aa590d239ee00
Parthbadar <parthbadar9@gmail.com>
2026-07-25 13:53:17 +0530

    dowhile loop

---
+++ dowhile11.java
@@ -0,0 +1,10 @@
+public class dowhile11 {
+    public static void main(String[] parth){
+        int i = 1;
+        do{
+            System.out.println("Hello parth and sarthak....");
+            System.out.println(i);
+            i+=1;
+        }while(i<=5);
+}
+}
