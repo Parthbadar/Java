b15fe2a12e9a338f64b7203f8a9cb5315ae38089
Parthbadar <parthbadar9@gmail.com>
2026-07-27 15:05:55 +0530

    while loop

---
+++ Whileloop10.java
@@ -0,0 +1,21 @@
+public class Whileloop10 {
+    public static void main(String[] parth){
+
+        // //while loop
+        // kuthn_start;
+
+        // while(condition){
+        //     block of code 
+        //     update;
+        // }
+
+
+
+        int j = 1;
+        while(j<=10){
+            System.out.println("hello parth and sarthak...");
+            System.out.println(j);
+            j+=1;
+        }
+}
+}
