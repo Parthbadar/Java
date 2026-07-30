e4fd5b4c7465bbf520308baafef8c75210f3db3d
Parthbadar <parthbadar9@gmail.com>
2026-07-30 19:20:09 +0530

    arraydemo

---
+++ ArrayDemo12.java
@@ -0,0 +1,16 @@
+public class ArrayDemo12 {
+    public static void main(String[] args) {
+        String [] friends = {
+            "suraj",
+            "parth",
+            "sarthak",
+            "sumit",
+            "jayesh",
+            "piyush"
+        };
+
+        for(int index = 0; index < friends.length; index++){
+            System.out.println(friends[index]);
+        }
+    }
+}
