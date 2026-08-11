a32bc96e2a12297ba1a27c400162f143bf747159
Parthbadar <parthbadar9@gmail.com>
2026-08-11 17:29:31 +0530

    Function with parameter and return value

---
+++ Function.java
@@ -0,0 +1,47 @@
+//1. Function with parameter and return value  
+public class Function {
+    // Function Definition
+    public static int addition(int a, int b)
+    {
+        return a + b;
+    }
+    public static void main(String[] args)
+    {       
+
+        int result = addition(10, 20);
+
+        System.out.println(result);
+
+        System.out.println(addition(2, 30));
+    }
+}
+ /*
+        
+        Types of function
+
+        1. predefined function
+           println(), print(), printf(), main()
+
+        2. userdefined function
+           parth(), suraj(), sarthak()
+
+        2. userdefined function
+           parth(), suraj(), sarthak()
+
+        Syntax
+
+                    a = 10, b = 20
+
+        datatype_name function_name(parameterlist)
+        {
+            block of code
+        }
+
+
+        1. Function with parameter and return value
+
+        2. Function no parameter and display DATA
+
+        3. Function with parameter and display DATA
+
+        */
