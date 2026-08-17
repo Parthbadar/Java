13fe387ace90eec1dae3cd1a4c73e034268836f3
Parthbadar <parthbadar9@gmail.com>
2026-08-18 01:10:22 +0530

    Recursion topic

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static int fact(int n) {
+        if(n < 2) {
+            return 1;
+        } else {
+            return n * fact(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("5 Factorial  Number -->" + fact(5));
+        
+    }
+}
