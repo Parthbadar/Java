c07d8d786fbc3286b20de6e103b23709ab42b087
Parthbadar <parthbadar9@gmail.com>
2026-08-18 22:34:06 +0530

    Recursion topic

--- Recursion.java
+++ Recursion.java
@@ -7,7 +7,7 @@ public class Recursion {
         }
     }
     public static void main(String[] args) {
-        System.out.println("5 Factorial  Number -->" + fact(5));
+        System.out.println("5 Factorial  Number --->" + fact(5));
         
     }
 }
