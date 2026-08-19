bfb35e1898c50b050c6b460c530246bcf4b3e181
Parthbadar <parthbadar9@gmail.com>
2026-08-19 16:29:41 +0530

    Opps concept 1.class and object

---
+++ ClassObject.java
@@ -0,0 +1,32 @@
+class person{
+    public int id;
+    public String name;
+    public String address;
+
+    public void setData(int pId, String pName, String pAddress){
+        id = pId;
+        name = pName;
+        address = pAddress;
+    }
+
+    public void showData(){
+        System.out.println("Student name =" + name);
+        System.out.println("Student id = " + id);
+        System.out.println("Student address = " + address);
+    }
+};
+public class ClassObject{
+    public static void main(String[] args) {
+        person p1 = new person();
+        p1.setData(1001, "parth", "NSK");
+
+        person p2 = new person();
+        p2.setData(1002, "Sarthak", "NSK");
+
+        p1.showData();
+        p2.showData();
+        
+        
+    }
+}
+
